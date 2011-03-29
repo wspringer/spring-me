4c2e5e683810e18eb8ec42186c7198df550f7f34
Martin Ellis <martin@ellis.name>
2011-03-29 11:45:38 +0100

    Ensure type information for each sink's source is known.
    The type and primitive flag for each MutableInstanceReference is copied
    from the MutableInstance to which it refers.
    The availability of this type information is a prerequisite for FactoryBean
    support.

--- spring-me-core/src/main/java/me/springframework/di/spring/QDoxAugmentation.java
+++ spring-me-core/src/main/java/me/springframework/di/spring/QDoxAugmentation.java
@@ -38,10 +38,12 @@
 package me.springframework.di.spring;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import me.springframework.di.MapSource;
 import me.springframework.di.Source;
+import me.springframework.di.base.AbstractSink;
 import me.springframework.di.base.MutableConstructorArgument;
 import me.springframework.di.base.MutableContext;
 import me.springframework.di.base.MutableInstance;
@@ -119,6 +121,8 @@ public class QDoxAugmentation implements Augmentation {
      */
     private void attribute(MutableInstance instance, MutableContext context) {
         guaranteeType(instance, context);
+        guaranteeTypes(context, instance.getConstructorArguments());
+        guaranteeTypes(context, instance.getSetters());
         logger.logAttributing(instance.getName());
         JavaClass cl = builder.getClassByName(instance.getType());
         if (isFactoryBean(cl)) {
@@ -195,6 +199,10 @@ public class QDoxAugmentation implements Augmentation {
      */
     private void guaranteeType(MutableInstance instance, MutableContext context) {
         logger.logGuaranteeingTypeKnown(instance.getId(), instance.getType() == null);
+        if (instance.getType() != null) {
+            return;
+        }
+
         // In case of no factory method
         if (instance.getFactoryMethod() == null) {
             instance.setType(instance.getReferencedType());
@@ -224,6 +232,42 @@ public class QDoxAugmentation implements Augmentation {
                 JavaMethod method = findMethod(factoryClass, false, instance.getFactoryMethod(), arguments);
                 logger.logFoundFactory(instance.getId(), factoryInstance.getId(), instance.getFactoryMethod(), method.getReturns().toString());
                 instance.setType(method.getReturns().toString());
+            }
+        }
+    }
+
+    /**
+     * Ensures that the type of the source is known for every sink.
+     *
+     * @param context The context for resolving references.
+     * @param sinks The sinks for which source types should be resolved.
+     */
+    private void guaranteeTypes(MutableContext context, Collection<? extends AbstractSink> sinks) {
+        if (sinks == null) {
+           return;
+        }
+        for (AbstractSink sink : sinks) {
+            Source source = sink.getSource();
+            if (source.getType() == null) {
+                guaranteeType(context, source);
+            }
+        }
+    }
+
+    /**
+     * Ensures that the type of the given source is known.
+     *
+     * @param context The context for resolving references.
+     * @param sinks The source that should have its type resolved.
+     */
+    private void guaranteeType(MutableContext context, Source source) {
+        if (source instanceof MutableInstanceReference) {
+            MutableInstanceReference ref = (MutableInstanceReference) source;
+            MutableInstance referent = context.getByName(ref.getName());
+            if (referent != null) {
+                guaranteeType(referent, context);
+                ref.setType(referent.getType());
+                ref.setPrimitive(referent.isPrimitive());
             }
         }
     }
